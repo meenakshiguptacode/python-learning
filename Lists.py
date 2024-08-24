5076c430d627a54cc1c8303ce753d394e6187cf6
Meenakshi.Gupta <meenakshi.gupta@srijan.net>
2024-08-24 15:59:07 +0530

    Added list intro

---
+++ Lists.py
@@ -0,0 +1,21 @@
+# list is a built in data type that stores set of values 
+# it can store elements of different types (integer , float , string etc etc )
+
+# marks = [87, 64, 33, 94, 76]
+# marks [0],marks [1]
+# student = ["karan", 87, "Delhi"]
+
+# assigment is alllowed in list
+#marks[0] = 67
+#len(marks)
+
+marks = [87.9, 64.9, 33.7, 94.4, 76.6]
+print(marks)
+print(type(marks))
+
+print(len(marks))
+print(marks[0])
+
+print(marks)
+marks[0] =  90
+print(marks)
