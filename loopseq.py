e1a640fc1730cb4c1191dfd32da9ad527c9c0409
Meenakshi.Gupta <meenakshi.gupta@srijan.net>
2024-09-03 22:25:21 +0530

    Added  loopseq file

---
+++ loopseq.py
@@ -0,0 +1,43 @@
+# loops in python
+# loops are used for sequential traversal . For traversing list , string , tuples
+
+# for loops 
+
+# for el in list:
+#   # some work 
+
+# list = [1,2,3]
+#  for el in list:
+#      print(el)  
+
+# for loops with else
+
+
+# for el in list:
+#   # some work 
+# else:
+#   # work when loop ends
+
+# list = [1,2,3]
+#  for el in list:
+#     print(el) 
+# else:
+#     print("END") 
+
+
+veg = ["peas", "patato" , "brinjal", "ladyfinger"]
+
+for val in veg:
+    print(val)
+    
+tup = (1,2,3,4,5,6,7,8)
+
+for val in tup:
+    print(val)
+
+str = "Meenakshi"
+
+for char in str:
+    print(char)
+else:
+    print("End")
